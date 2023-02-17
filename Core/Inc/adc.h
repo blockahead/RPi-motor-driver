5a664d601b262a5ec0baa78eb6be965716cdca5c
Kazuki Hamada <blockahead@gmail.com>
2023-02-17 21:00:21 +0900

    Modify function declaration in adc.h.

--- Core/Inc/adc.h
+++ Core/Inc/adc.h
@@ -13,7 +13,7 @@
 extern uint16_t *const adc_reg_addr;
 
 uint8_t adc_get_num_of_channels(void);
-void adc_set_reference_voltage(const uint8_t channel, float voltage);
+void adc_set_reference_voltage(const uint8_t channel, const float voltage);
 float adc_get_reference_voltage(const uint8_t channel);
 float adc_get_voltage(const uint8_t channel);
 
